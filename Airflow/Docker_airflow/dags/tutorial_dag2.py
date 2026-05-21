51875c61ed622d4b35108d1ab53b45005064c587
Andre Radatz <radatzwork@gmail.com>
2026-05-21 19:16:30 -0300

    airflow dag

---
+++ Airflow/Docker_airflow/dags/tutorial_dag2.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+import pandas as pd
+import requests
+import json
+
+def captura_conta_dados():
+    url = 'https://data.cityofnewyork.us/resource/rc75-m7u3.json'
+    response = requests.get(url)
+    df = pd.DataFrame(response.json())
+    qtd = len(df.index)
+    return qtd
+
+def e_valida(ti):
+    qtd = ti.xcom_pull(task_ids='captura_conta_dados')
+    if qtd > 1000:
+        return 'valido'
+    else:
+        return 'nvalido'
+
+with DAG('tutorial_dag2', start_date=datetime(2026, 1, 1), schedule='30 * * * *', catchup=False) as dag:
+    
+    captura_conta_dados = PythonOperator(
+        task_id='captura_conta_dados',
+        python_callable=captura_conta_dados
+    )
+
+    e_valida = BranchPythonOperator(
+        task_id='e_valida',
+        python_callable=e_valida
+    )
+
+    valido = BashOperator(
+        task_id='valido',
+        bash_command='echo "Quantidade de dados é maior que 10"'
+    )
+    
+    nvalido = BashOperator(
+        task_id='nvalido',
+        bash_command='echo "Quantidade de dados é menor que 10"'
+    )
+    
+    # Definir dependências
+    captura_conta_dados >> e_valida >> [valido, nvalido]
